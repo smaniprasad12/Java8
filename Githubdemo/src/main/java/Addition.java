2988e32b9d2516e9356ccc96a7480b60d7faa382
hp <hp@LAPTOP-1NS0I2D8>
2022-04-21 10:58:38 +0530

    BasicCodes

---
+++ Githubdemo/src/main/java/Addition.java
@@ -0,0 +1,14 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println(c);
+		
+	
+
+	}
+
+}
